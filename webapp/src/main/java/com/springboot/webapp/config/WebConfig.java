3c502a2b07493a43c9799394dd4ce1a069ca9cc4
gagan0183 <g.deepsingh1@gmail.com>
2020-06-05 01:51:10 +0530

    config added

---
+++ webapp/src/main/java/com/springboot/webapp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.springboot.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
---
+++ webapp/web/login.jsp
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form method="post">
+            <input type="text" name="name">
+            <input type="password" name="password">
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
