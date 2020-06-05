ad439ea54fd78cb3d1b908bc767485c8b345d88e
gagan0183 <g.deepsingh1@gmail.com>
2020-06-05 12:36:41 +0530

    todo model

---
+++ webapp/src/main/java/com/springboot/webapp/model/Todo.java
@@ -0,0 +1,77 @@
+package com.springboot.webapp.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Todo {
+    private int id;
+    private String user;
+    private String desc;
+    private Date targetDate;
+    private boolean isDone;
+
+    public Todo(int id, String user, String desc, Date targetDate, boolean isDone) {
+        this.id = id;
+        this.user = user;
+        this.desc = desc;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return id == todo.id &&
+                isDone == todo.isDone &&
+                user.equals(todo.user) &&
+                desc.equals(todo.desc) &&
+                targetDate.equals(todo.targetDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, desc, targetDate, isDone);
+    }
+}
